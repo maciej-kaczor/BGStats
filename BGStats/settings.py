4d10488f6dd3834698c9313df235c06391be5b03
Maciej Kaczor <maciej.kaczor92@gmail.com>
2018-11-29 20:50:59 +0100

    Boardgame app

--- BGStats/settings.py
+++ BGStats/settings.py
@@ -11,14 +11,16 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import secret_settings.py
+from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'gfy1u#o6e8vt3o+fbx^nuw@pjxl^$k-at(31iivwq6z%)!^3ay'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -67,6 +69,13 @@ WSGI_APPLICATION = 'BGStats.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
